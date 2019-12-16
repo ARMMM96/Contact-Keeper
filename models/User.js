ecaff6209ce75f5eb1a4b511fe2fba3dfbe40285
Ahmed Rabie Mahmoud <ahmed.rabie.mahmoud.96@gmail.com>
2019-12-16 05:22:28 +0200

    Adding registration functionality

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    date: {
+        type: Date,
+        defult: Date.now
+    },
+
+});
+
+module.exports = mongoose.model('user', UserSchema);
